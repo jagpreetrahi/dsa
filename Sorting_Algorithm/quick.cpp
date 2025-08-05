7eef3133f29156c47e3016d6866b6db6e0f3eaf8
jagpreetrahi <jagpreet1615@gmail.com>
2025-08-05 11:29:24 +0530

    Updates the code with some merge sort theory and the hoare partiton approac

--- Sorting_Algorithm/quick.cpp
+++ Sorting_Algorithm/quick.cpp
@@ -1,7 +1,52 @@
+/* Quick Sort is a sorting  algorithm that based on the divide and conqueror strategy. It uses the nlogn comparison. It is in-places algorithm
+  It does by selecting a pivot element , it can be --
+   1. first or last element
+   2. middle element
+   3. random element
+  
+   After placing the pivot to its correct position , all the smaller unsorted elements are to be places on its left side
+   and the greater elements on its right side.
+  
+
+  How it works -> 1. Select a pivot.
+        2. Rearrange the array i.e. all the smaller elements on its left and all greater elements on its right side.
+        3. Divide the array into subarray, until the size of an array is to a single element.
+        4. Repeat the process until the array is sorted.
+    
+    Time Complexity -> o(logn) - due to divide the array into sub array.
+        O(n) due to merging the blocks
+        Total TC -> O(n logn)
+    Space Complexity -> it does by recursion depth , not the array copying
+        best case or average -> O(n) due to pivot divide the array into two half leading to log n recursive call  .
+        worst case -> O(n) due to pivot always pick smaller or larger , leading to unbalanced partition  and n recursive call                           
+                
+*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
+// if the pivot is first element -> TC -> O(n)
+int firstElementPartition(vector<int> &arr, int low, int high){
+    int i = low; 
+    int j = high -1; 
+    int pivot = arr[low];
+    while(i<j){
+
+        while(arr[i] <= pivot && i <= high -1){
+            i++;
+        }
+
+        while(arr[j] > pivot && j >= low + 1){
+            j--;
+        }
+        if(i<j) swap(arr[i] , arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+// Lomuto Partition Schema
 int partition(vector<int> &arr, int low, int high){
 
     int pivot = arr[high];
